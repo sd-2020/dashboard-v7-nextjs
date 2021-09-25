f1f444cb8bd0e6f88a4e6ef3a2f78f1b0759bad2
enochndika <enochbeloved@gmail.com>
2021-09-25 20:14:41 +0100

    fix linter bugs, update dependencies and prettier extension to json

--- src/dashboard/provider/context.js
+++ src/dashboard/provider/context.js
@@ -13,11 +13,13 @@ export default function DashboardProvider({ children }) {
     setOpen((prevState) => !prevState);
   }, []);
 
-  // set the html tag overflow to hidden
-  // close side navigation on mobile when route starts changing
-  // it's triggered when viewport is less than 1024px
+  // set the html tag style overflow to hidden
   React.useEffect(() => {
     document.documentElement.style.overflow = 'hidden';
+  }, []);
+
+  // close side navigation on mobile when route changes. it's triggered when viewport is less than 1024px
+  React.useEffect(() => {
     if (open && window.innerWidth < 1024) {
       router.events.on('routeChangeStart', () => setOpen(false));
     }
